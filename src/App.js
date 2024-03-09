bfd1ebfd575c5d1f94f21e833e83b92f922fd46e
AjokeCode <ruqayyahalabi536@gmail.com>
2024-03-09 19:27:35 +0100

    Dammy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import About from './components/about/about';
 import Contact from './components/contact/contact';
 import Hero from './components/hero/hero';
-import Navbar from './components/navbar/navbar';
+import Header from './components/navbar/navbar';
+
 import Project from './components/project/project';
 // import ParticlesBackground from './components/particles';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       
-      <Navbar/>
+      <Header/>
       <Hero/>
       <About/>
       <Project/>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,52 +5,45 @@ import img from './4.png';
 import { BsListNested } from 'react-icons/bs';
 import { AiOutlineClose } from 'react-icons/ai';
 
-
-const Navbar = ()=>{
-    const [navbar, setNavbar] = useState(false);
-    const [isClick, setIsClick]= useState(false);
-    function handleScroll() {
-        if (window.scrollY >= 80) {
-            setNavbar(true)
-        } else {
-            setNavbar(false)
-        }
-    }
-    window.addEventListener('scroll', handleScroll)
-    
-    return (
-        <>        
-        <header className={navbar ? "navbar active" : "navbar"}>
-            <div className="navbar-left">
-            <img src={img} alt='img' className="navbar-left-img"/>
-            <h1 className="navbar-left-header">Ajoke_Code</h1>
+const Header = ()=>{
+    const [isClick, setIsClick] = useState(false)
+    return(
+        <header className="header">
+            <div className="header-1">
+                <img src={img} alt='img' className='header-1-img'/>
+            <h1 className="header-1-title">
+                Ajokecode
+            </h1>
             </div>
-            <ul className="navbar-list">
-                <li className="navbar-item"><Link to="home" spy={true} smooth={true}  duration={500} className="navbar-link">Home</Link></li>
-                <li className="navbar-item"><Link to="about" spy={true} smooth={true} offset={-150} duration={500} className="navbar-link">About</Link></li>
-                <li className="navbar-item"><Link to="project" spy={true} smooth={true} offset={-100} duration={500} className="navbar-link">Projects</Link></li>
-                <li className="navbar-item"><Link to="contact" spy={true} smooth={true} offset={-100} duration={500} className="navbar-link">Contact me</Link></li>
+            <ul className="header-list">
+                <li className="header-item"><Link to="home" spy={true} smooth={true} offset={-100} duration={500} className="header-link">Home</Link></li>
+                <li className="header-item"><Link to="about" spy={true} smooth={true} offset={-150} duration={500} className="header-link">About</Link></li>
+                <li className="header-item"><Link to="features" spy={true} smooth={true} offset={-150} duration={500} className="header-link">Features</Link></li>
+                <li className="header-item"><Link to="download" spy={true} smooth={true} offset={-100} duration={500} className='header-link'>Download</Link></li>
             </ul>
             
             <BsListNested
-              className= 'open'
-              onClick={() => setIsClick(true)} />
-      {
-        isClick ? (
-          <div className='ham-div'>
-            <AiOutlineClose className="close" onClick={() => setIsClick(false)} />
-            <ul className="navbar-lists">
-            <li className="navbar-items" ><Link to="home" spy={true} smooth={true}  duration={500} className="navbar-links">Home</Link></li>
-            <li className="navbar-items"><Link to="about" spy={true} smooth={true} offset={-150} duration={500} className="navbar-links">About</Link></li>
-            <li className="navbar-items"><Link to="project" spy={true} smooth={true} offset={-100} duration={500} className="navbar-links">Projects</Link></li>
-            <li className="navbar-items"><Link to="contact" spy={true} smooth={true} offset={-100} duration={500} className="navbar-links">Contact me</Link></li>
-            </ul>
-        </div>
-        ) : (<></>)
-      }
-      </header>
-      </>
-)
-}
-export default Navbar;
+              className={`${isClick ? 'none' : ''} open`}
+              onClick={() => setIsClick(true)}
+      />
+          {
+            isClick ? (
+              <div className='ham-div'>
+          <AiOutlineClose
+              className="close"
+              onClick={() => setIsClick(false)}
+            />
+            <ul className='header-lists'>
+              <li className='header-items'><Link to="hero" spy={true} smooth={true} offset={-100} duration={500} className='header-links'>Home</Link></li>
+              <li className='header-items'><Link to="about" spy={true} smooth={true} offset={-150} duration={500} className='header-links'>About</Link></li>
+              <li className='header-items'><Link to="features" spy={true} smooth={true} offset={-150} duration={500} className='header-links'>Features</Link></li>
+              <li className='header-items'><Link to="download" spy={true} smooth={true} offset={-100} duration={500} className='header-links'>Download</Link></li>
 
+            </ul>
+          </div>
+            ) : (<></>)
+          }
+        </header>
+    )
+}
+export default Header;
